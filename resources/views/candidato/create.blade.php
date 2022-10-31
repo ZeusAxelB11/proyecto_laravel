123a4cee1488fc0b0532225f9b13c734f53b512e
Zeus Axel Bustos Alavez <zeusss.za@gmail.com>
2022-10-31 14:03:38 -0600

    commit 31-10-22

--- resources/views/candidato/create.blade.php
+++ resources/views/candidato/create.blade.php
@@ -1,10 +1,6 @@
 @extends('plantilla')
 @section('content')
-<style>
-    .uper {
-        margin-top: 40px;
-    }
-</style>
+
 <div class="card uper">
     <div class="card-header">
         Agregar Candidato
@@ -21,44 +17,43 @@
         @endif
         <form method="post" 
             action="{{ route('candidato.store') }} " 
-            enctype="multipart/form-data"
-            onsubmit="return validateData();">
-            {{ csrf_field() }}
+            enctype="multipart/form-data" 
+            onsubmit="return validateType('perfil', 'aplication/pdf');">
+            @csrf
             <div class="form-group">
-                <label for="nombrecompleto">Nombre completo:</label>
-                <input type="text" id="nombrecompleto"
-                 class="form-control" name="nombrecompleto" />
+                <label for="nombrecompleto">Nombre:</label>
+                <input type="text" class="form-control" 
+                    id="nombrecompleto"
+                    name="nombrecompleto" />
             </div>
             <div class="form-group">
                 <label for="sexo">Sexo:</label>
-                <select name="sexo">
+                <select name="sexo" id="sexo">
+                    <option value="M" selected>Mujer</option>
                     <option value="H">Hombre</option>
-                    <option value="M">Mujer</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="foto">Foto:</label>
-                <input type="file" id="foto" accept="image/png, image/jpeg" 
-                 class="form-control" name="foto" 
-                 onchange="previewImage(event,'imageCandidato');"
-                  />
-                  <img src="" id="imageCandidato" width="200px" heigth="200px">
+                <input type="file" id="foto" name="foto" 
+                accept="image/png, image/jpeg" 
+                onchange="preview(event,'previewImage');"
+                >
+                <div id="previewImage"></div>
             </div>
             <div class="form-group">
                 <label for="perfil">Perfil:</label>
-                <input type="file" id="perfil" accept="application/pdf"
-                class="form-control" name="perfil" onchange="return validarExt()"/>
-            </div>
-            <br><br>
-			<div id="visorArchivo">
-				<!--Aqui se desplegará el fichero-->
-			</div>
-
-            <button type="submit" 
-            class="btn btn-primary">Guardar</button>
+                <input type="file" id="perfil" name="perfil" 
+                accept="application/pdf"
+                onchange="preview(event,'previewPDF');"
+                >
+                <div id="previewPDF"></div>
+            </div>            
+            <button type="submit" class="btn btn-primary">Guardar</button>
         </form>
     </div>
 </div>
-<script type="text/javascript" src="{{ asset('js/custom.js') }}">
+<script type="text/javascript" 
+src="{{ URL::asset('js/custom.js') }}">
 </script>
 @endsection
